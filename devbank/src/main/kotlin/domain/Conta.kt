d32140a8cc1611802bb0f6727d157b2032be0f11
VitorSB <vitor.s.barbosa@kroton.com.br>
2023-04-27 22:52:50 -0300

    Implementando métodos e alterando main

--- devbank/src/main/kotlin/domain/Conta.kt
+++
@@ -1,7 +0,0 @@
-package domain
-
-class Conta {
-    var titular: String = ""
-    var numero: Int = 0
-    var saldo: Double = 0.0
-}
--- devbank/src/main/kotlin/main.kt
+++ devbank/src/main/kotlin/main.kt
@@ -1,9 +1,16 @@
-import domain.Conta
+import model.Conta
 
-fun main(){
+fun main() {
     println("Bem vindo ao DevBank!")
 
-    val contaVitor = Conta()
-    contaVitor.titular = "Vitor"
+    val contaVitor = Conta("Vitor", 1000)
+    contaVitor.deposita(1000.0)
+
+    val contaRex = Conta("Rex", 1001)
+    contaRex.deposita(1000.0)
+
+    contaVitor.transfere(contaRex, 10.0)
+    println(contaVitor.saldo)
+    println(contaRex.saldo)
 
 }
